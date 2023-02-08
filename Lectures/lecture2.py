dd231e2cdbc3cee33d863ef205259effcf59e79b
Айкин Иван <ivan.aykin@mail.ru>
2023-02-08 21:43:52 +0700

    added practice 3

---
+++ Lectures/lecture2.py
@@ -0,0 +1,78 @@
+# Список
+# list_1 = []
+# list_1 = list()
+# list_1 = [1, 2, 3, 4, 5]
+# print(list_1)
+
+# list_1 = [5, 1]
+# print(list_1)
+# list_1.append(8)
+# print(list_1)
+
+# list_1 = []
+# print(list_1)
+# for i in range(5):
+#     list_1.append(i)
+#     print(*list_1)
+
+# list_1 = [12, 2, 33, 2, 43, 41]
+# print(list_1)
+# # list_1.pop()
+# # print(list_1)
+# # print(list_1.pop())
+# # print(list_1)
+# # print(list_1.pop(2))
+# # print(list_1)
+# print(list_1.insert(2, 2))
+# print(list_1)
+
+# Кортежи - tuple - неизменяемые списки
+
+# v = [1, 2, 3]
+# print(v)
+# print(type(v))
+
+# v = tuple(v)
+# print(v)
+# print(type(v))
+
+# a, b, c = v
+# print(a, b, c)
+
+#  Словарь 
+
+# d = {}
+
+# d['q'] = 'qwerty'
+# print(d)
+# d['w'] = 'werty'
+# print(d)
+
+# Множество содержит только уникальные значения
+
+# colors = {'red', 'green', 'black'}
+# print(colors)
+# colors.add('gray')
+# print(colors)
+# colors.remove('red') - удаляет определенный элемент
+# colors.discard('red') - сперва проверяет есть ли данный элемент в множестве и если есть, то удаляет
+# color.clear() - удаляет все элементы из множества
+# q = set() - создает множество
+
+# Операции с множествами
+
+# a.copy() - копирует множество
+# c = a - 1вое множество.union(2рое множество) - обьединяет два множества
+# a.interscription() - пересечение множества
+# a.difference(b) - разность, вычетаем из множества а множество б
+# frozenset(a) - замораживает множество, делает его не редактируемым
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+
